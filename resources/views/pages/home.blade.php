d658a75aa386dcd8b129a4e1f3b6da1aac7f7779
Shae Smith <sbsmith86@gmail.com>
2018-03-29 16:57:02 -0400

    Adding chompy

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,16 @@
 @section('main_content')
 
 <div class="jumbotron">
-    <h1>Welcome to Chompy</h1>
-    <p>The DoSomething.org Importer app</p>
-</p>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="https://media.giphy.com/media/HEkBXK8qnIRuo/giphy.gif" width="200" alt="chompy" class="img-rounded">
+        </div>
+        <div class="col-md-8">
+            <h1>Welcome to Chompy</h1>
+            <p>The DoSomething.org Importer app</p>
+            <p>Go ahead...import a CSV</p>
+        </div>
+    </div>
 </div>
 
 @stop
