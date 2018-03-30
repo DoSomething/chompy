c5635aeab7cce47ea5f0fa5c5494357a374f14ac
Shae Smith <sbsmith86@gmail.com>
2018-03-30 11:04:54 -0400

    Make it look nicer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,10 +5,10 @@
 <div class="jumbotron">
     <div class="row">
         <div class="col-md-4">
-            <img src="https://media.giphy.com/media/HEkBXK8qnIRuo/giphy.gif" width="200" alt="chompy" class="img-rounded">
+            <img src="https://media.giphy.com/media/HEkBXK8qnIRuo/giphy.gif" width="300" alt="chompy" class="img-rounded center-block">
         </div>
         <div class="col-md-8">
-            <h1>Welcome to Chompy</h1>
+            <h1 class="media-heading">Welcome to Chompy</h1>
             <p>The DoSomething.org Importer app</p>
             <p>Go ahead...import a CSV</p>
         </div>
