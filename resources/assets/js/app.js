5275cf4c496acf3f784aee839079b6d3993b4847
Shae Smith <sbsmith86@gmail.com>
2018-06-07 15:29:23 -0400

    reorganize js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,28 +1,8 @@
-import { ready } from './helpers';
 import './bootstrap';
-import fileUpload from './forms/fileUpload';
 
-// @TODO - this was stolen from https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3 figure out better file input styling.
-ready(() => {
-    $(document).on('change', ':file', function () {
-        var input = $(this),
-            numFiles = input.get(0).files ? input.get(0).files.length : 1,
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label]);
-    });
+// Handles the upload form interaction
+import './uploadForm';
 
-    $(document).ready(function () {
-        $(':file').on('fileselect', function (event, numFiles, label) {
+// Handles updating the progress bar
+import './progress';
 
-            var input = $(this).parents('.input-group').find(':text'),
-                log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-            if (input.length) {
-                input.val(log);
-            } else {
-                if (log) alert(log);
-            }
-
-        });
-    });
-});
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,3 @@
-
-window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -9,8 +6,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
@@ -43,31 +38,31 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo'
+// import Echo from 'laravel-echo'
 
-window.Pusher = require('pusher-js');
+// window.Pusher = require('pusher-js');
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'b0f937b1f525e790847e',
-    cluster: 'us2',
-    encrypted: true
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'b0f937b1f525e790847e',
+//     cluster: 'us2',
+//     encrypted: true
+// });
 
-// Listen to the 'LogProgress Event' event on the importer channel.
-window.Echo.channel('importer')
-    .listen('LogProgress', (e) => {
-        const messageType = e.type;
-        const messageContent = e.message;
-        const value = e.progressValue;
+// // Listen to the 'LogProgress Event' event on the importer channel.
+// window.Echo.channel('importer')
+//     .listen('LogProgress', (e) => {
+//         const messageType = e.type;
+//         const messageContent = e.message;
+//         const value = e.progressValue;
 
-        // @TODO - don't use jquery
-        if (messageType == 'general') {
-            $('#messages').append('<code>' + e.message + '</code>\n');
-        }
+//         // @TODO - don't use jquery
+//         if (messageType == 'general') {
+//             $('#messages').append('<code>' + e.message + '</code>\n');
+//         }
 
-        if (messageType == 'progress') {
-            $('.progress-bar').attr("aria-valuenow", value);
-            $('.progress-bar').attr('style', 'width: ' + value + '%');
-        }
-    });
+//         if (messageType == 'progress') {
+//             $('.progress-bar').attr("aria-valuenow", value);
+//             $('.progress-bar').attr('style', 'width: ' + value + '%');
+//         }
+//     });
