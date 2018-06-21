679e0582299071a174e48fc47085b39dc9dac420
deguzmankevin <deguzmankevin@hotmail.com>
2018-06-21 14:29:08 -0400

    fix northstar environment prompt insetupcommand

--- app/Console/Commands/SetupCommand.php
+++ app/Console/Commands/SetupCommand.php
@@ -35,8 +35,8 @@ class SetupCommand extends Command
         $this->section('Set Northstar environment variables', function () {
             $environments = [
                 'http://northstar.dev' => 'http://aurora.dev',
-                'https://northstar-qa.dosomething.org' => 'https://aurora-qa.dosomething.org',
-                'https://northstar-thor.dosomething.org' => 'https://aurora-thor.dosomething.org',
+                'https://identity-dev.dosomething.org' => 'https://aurora-qa.dosomething.org',
+                'https://identity-qa.dosomething.org' => 'https://aurora-thor.dosomething.org',
             ];
 
             $this->chooseEnvironmentVariable('NORTHSTAR_URL', 'Choose a Northstar environment', array_keys($environments));
@@ -44,8 +44,8 @@ class SetupCommand extends Command
             $this->instruction('You can get these environment variables from Aurora\'s "Clients" page:');
             $this->instruction($environments[env('NORTHSTAR_URL')] . '/clients');
 
-            $this->setEnvironmentVariable('NORTHSTAR_AUTH_ID', 'Enter the OAuth Client ID');
-            $this->setEnvironmentVariable('NORTHSTAR_AUTH_SECRET', 'Enter the OAuth Client Secret');
+            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_ID', 'Enter the OAuth Client ID');
+            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_SECRET', 'Enter the OAuth Client Secret');
         });
 
         $this->runCommand('key:generate', 'Creating application key');
