9734ee1437f8b29e1289caf244a01d6817384a8a
David Furnes <david@dfurnes.com>
2018-07-03 15:00:08 -0400

    Update setup command with new Aurora URLs.

--- app/Console/Commands/SetupCommand.php
+++ app/Console/Commands/SetupCommand.php
@@ -35,8 +35,8 @@ class SetupCommand extends Command
         $this->section('Set Northstar environment variables', function () {
             $environments = [
                 'http://northstar.test' => 'http://aurora.test',
-                'https://identity-dev.dosomething.org' => 'https://aurora-qa.dosomething.org',
-                'https://identity-qa.dosomething.org' => 'https://aurora-thor.dosomething.org',
+                'https://identity-dev.dosomething.org' => 'https://admin-dev.dosomething.org',
+                'https://identity-qa.dosomething.org' => 'https://admin-qa.dosomething.org',
             ];
 
             $this->chooseEnvironmentVariable('NORTHSTAR_URL', 'Choose a Northstar environment', array_keys($environments));
@@ -44,36 +44,29 @@ class SetupCommand extends Command
             $this->instruction('You can get these environment variables from Aurora\'s "Clients" page:');
             $this->instruction($environments[env('NORTHSTAR_URL')] . '/clients');
 
-            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_ID', 'Enter the OAuth Client ID');
-            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_SECRET', 'Enter the OAuth Client Secret');
-            $this->setEnvironmentVariable('NORTHSTAR_CLIENT_ID', 'Enter the Northstar Client ID');
-            $this->setEnvironmentVariable('NORTHSTAR_CLIENT_SECRET', 'Enter the Northstar  Client Secret');
+            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_ID', 'Enter the OAuth Client ID for web sessions');
+            $this->setEnvironmentVariable('NORTHSTAR_AUTHORIZATION_SECRET', 'Enter the OAuth Client Secret for web sessions');
+            $this->setEnvironmentVariable('NORTHSTAR_CLIENT_ID', 'Enter the OAuth Client ID for machine requests');
+            $this->setEnvironmentVariable('NORTHSTAR_CLIENT_SECRET', 'Enter the OAuth Client Secret for machine requests');
         });
 
 
         $this->section('Set Rogue environment variables', function () {
             $environments = [
+                'http://rogue.test',
                 'https://rogue-qa.dosomething.org',
                 'https://rogue-thor.dosomething.org'
             ];
 
             $this->chooseEnvironmentVariable('ROGUE_URL', 'Choose a Rogue environment', $environments);
-
         });
 
         $this->section('Set Pusher environment variables', function () {
             $this->setEnvironmentVariable('PUSHER_APP_ID', 'Enter the Pusher App ID');
             $this->setEnvironmentVariable('PUSHER_APP_KEY', 'Enter the Pusher App Key');
             $this->setEnvironmentVariable('PUSHER_APP_SECRET', 'Enter the Pusher App Secret');
-
         });
         
-        $this->section('Set Filesystem environment variables', function () {
-            $this->instruction('Be sure to set this to local if you are testing locally!');
-            $this->setEnvironmentVariable('FILESYSTEM_DRIVER', 'Enter the filesystem driver');
-           
-
-        });
         $this->runCommand('key:generate', 'Creating application key');
 
         $this->runCommand('gateway:key', 'Fetching public key from Northstar');
