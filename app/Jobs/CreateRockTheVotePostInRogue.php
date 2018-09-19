9fe91e2c31b67df077148f48659e401fcdc924f5
Chloe Lee <chloealessandra@gmail.com>
2018-09-19 15:01:52 -0400

    updates job name

--- app/Jobs/CreateRockTheVotePostInRogue.php
+++ app/Jobs/CreateRockTheVotePostInRogue.php
@@ -9,7 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class CreateTurboVotePostInRogue implements ShouldQueue
+class CreateRockTheVotePostInRogue implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, ImportToRogue;
 
