8f06b797459b02754c3ee6c5fd3842454aa42886
Chloe Lee <chloealessandra@gmail.com>
2018-09-19 15:01:26 -0400

    creates CreateRockTheVotePostInRogue job

---
+++ app/Jobs/CreateRockTheVotePostInRogue.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Chompy\Jobs;
+
+use Chompy\Services\Rogue;
+use Illuminate\Bus\Queueable;
+use Chompy\Traits\ImportToRogue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class CreateTurboVotePostInRogue implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, ImportToRogue;
+
+    /**
+     * The record to be created into a post from the csv.
+     *
+     * @var array
+     */
+    protected $record;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($record)
+    {
+    	$this->record = $record;
+    }
+
+    /**
+     * Execute the job to create a Turbo Vote post in Rogue.
+     *
+     * @return array
+     */
+    public function handle(Rogue $rogue)
+    {
+
+    }
+
+}
