b081aa63aca05fe192eb42312eea31c60d76113d
Aaron Schachter <aaronschachter@gmail.com>
2019-04-02 10:07:34 -0700

    Adds down function per https://github.com/DoSomething/chompy/pull/82#discussion_r271377499

--- database/migrations/2019_04_01_000000_drop_password_resets_and_stats_tables.php
+++ database/migrations/2019_04_01_000000_drop_password_resets_and_stats_tables.php
@@ -15,4 +15,25 @@ class RemovePasswordResetsAndStatsTables extends Migration
         Schema::dropIfExists('password_resets');
         Schema::dropIfExists('stats');
     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+        Schema::create('stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('total_records');
+            $table->text('filename')->after('id');
+            $table->text('stats')->nullable();
+            $table->timestamps();
+        });
+    }
 }
