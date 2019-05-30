35665ae684fb2caaa13d0cc542c7f146b2a09f23
Chloe Lee <chloealessandra@gmail.com>
2019-05-30 10:03:42 -0400

    reindents

--- app/Http/Middleware/AuthenticateWithApiKey.php
+++ app/Http/Middleware/AuthenticateWithApiKey.php
@@ -7,36 +7,36 @@ use Illuminate\Auth\AuthenticationException;
 
 class AuthenticateWithApiKey
 {
-  protected $keys;
+    protected $keys;
 
-  public function __construct()
-  {
-    $this->keys = [
+    public function __construct()
+    {
+        $this->keys = [
       'X-DS-Importer-API-Key' => config('app.callpower_key'),
       'X-DS-CallPower-API-Key' => config('app.callpower_key'),
       'X-DS-SoftEdge-API-Key' => config('app.softedge_key'),
     ];
-  }
-
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   * @param  $headers
-   * @return mixed
-   *
-   * @throws \Illuminate\Auth\AuthenticationException
-   */
-  public function handle($request, Closure $next, ...$headers)
-  {
-    // Check the header for authorization.
-    foreach ($headers as $header) {
-        if ($request->header($header) === $this->keys[$header]) {
-            return $next($request);
-        }
     }
 
-    throw new AuthenticationException;
-  }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  $headers
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$headers)
+    {
+        // Check the header for authorization.
+        foreach ($headers as $header) {
+            if ($request->header($header) === $this->keys[$header]) {
+                return $next($request);
+            }
+        }
+
+        throw new AuthenticationException;
+    }
 }
