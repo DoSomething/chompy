fe101ec840c79b6787960fd4e4043de6cf851ba5
Chloe Lee <chloealessandra@gmail.com>
2019-06-18 10:52:32 -0400

    updates importfile and importemailsubscription to add first name when creating or updating a user

--- app/Jobs/ImportFile.php
+++ app/Jobs/ImportFile.php
@@ -107,7 +107,7 @@ class ImportFile implements ShouldQueue
                 ImportRockTheVoteRecord::dispatch($record);
             }
             if ($this->importType === ImportType::$emailSubscription) {
-                ImportEmailSubscription::dispatch($record['email'], $this->importOptions['source_detail'], $this->importOptions['email_subscription_topics']);
+                ImportEmailSubscription::dispatch($record['email'], $record['first_name'], $this->importOptions['source_detail'], $this->importOptions['email_subscription_topics']);
             }
             event(new LogProgress('', 'progress', ($offset / $this->totalRecords) * 100));
         }
