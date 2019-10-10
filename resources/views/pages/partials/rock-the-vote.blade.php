bad5c7360fa370b2f3833606ec62ae761b9d34a0
katiecrane <kcrane@dosomething.org>
2019-10-10 15:49:04 -0700

    Display the user source_detail on the import page

--- resources/views/pages/partials/rock-the-vote.blade.php
+++ resources/views/pages/partials/rock-the-vote.blade.php
@@ -16,6 +16,15 @@
     </div>
 </div>
 <div class="form-group row">
+    <label class="col-sm-3 col-form-label">User source detail</label>
+    <div class="col-sm-9">
+        <p class="form-control-static"><code>{{ $config['user']['source_detail'] }}</code></p>
+        <small class="form-text text-muted">
+          The source details we will store on new users.
+        </small>
+    </div>
+</div>
+<div class="form-group row">
     <label class="col-sm-3 col-form-label">Send Password Reset</label>
     <div class="col-sm-9">
         <p class="form-control-static">{{ $config['reset']['enabled'] ? 'ON' : 'OFF' }}</p>
