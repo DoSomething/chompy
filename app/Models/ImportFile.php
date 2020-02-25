1f3f8691b679bbc554d0644e4522e054adca9807
Aaron Schachter <aaronschachter@gmail.com>
2020-02-25 07:16:50 -0800

    Style fix, adds indexes

--- app/Models/ImportFile.php
+++ app/Models/ImportFile.php
@@ -12,9 +12,21 @@ class ImportFile extends Model
      * @var array
      */
     protected $fillable = [
-      'filepath',
-      'import_type',
-      'row_count',
-      'user_id',
+        'filepath',
+        'import_type',
+        'row_count',
+        'user_id',
+    ];
+
+    /**
+     * Attributes that can be queried when filtering.
+     *
+     * This array is manually maintained. It does not necessarily mean that
+     * any of these are actual indexes on the database... but they should be!
+     *
+     * @var array
+     */
+    public static $indexes = [
+        'import_type',
     ];
 }
