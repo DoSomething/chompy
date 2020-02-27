166707888e58dc7b5d6dc1ddff22ba4d6fb94c1d
Aaron Schachter <aaronschachter@gmail.com>
2020-02-27 12:05:08 -0800

    Fix spacing

--- resources/views/pages/import-files/show.blade.php
+++ resources/views/pages/import-files/show.blade.php
@@ -7,7 +7,7 @@
         {{$importFile->created_at}}
     </h1>
     <p>
-          <strong>{{$importFile->import_type}}</strong> ({{$importFile->row_count}} rows)
+        <strong>{{$importFile->import_type}}</strong> ({{$importFile->row_count}} rows)
     </p>
     @if ($importFile->import_type === \Chompy\ImportType::$rockTheVote)
         @include('pages.partials.rock-the-vote.logs', ['rows' => $rows])
