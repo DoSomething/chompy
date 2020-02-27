bedddab1d62ace112cdaf390d3fc230dca4068ea
Aaron Schachter <aaronschachter@gmail.com>
2020-02-27 11:17:16 -0800

    Explode tracking source

--- resources/views/pages/import-files/show.blade.php
+++ resources/views/pages/import-files/show.blade.php
@@ -3,16 +3,19 @@
 @section('main_content')
 
 <div>
-  <h1>
-    {{$importFile->filepath}}
-  </h1>
+    <h1>
+      {{$importFile->created_at}}
+    </h1>
+    <p>
+      Total rows: {{$importFile->row_count}}
+    </p>
     <table class="table">
         <thead>
           <tr class="row">
             <th class="col-md-2">Started Registration</th>
             <th class="col-md-2">User</th>
             <th class="col-md-1">Status</th>
-            <th class="col-md-3">Tracking Code</th>
+            <th class="col-md-3">Tracking Source</th>
             <th class="col-md-2">Finish With State</th>
             <th class="col-md-2">Pre-Registered</th>
 
@@ -30,7 +33,11 @@
                 {{$row->status}}
               </td>
               <td class="col-md-3">
-                {{$row->tracking_source}}
+                <ul>
+                  @foreach(explode(',', $row->tracking_source) as $key => $value)
+                    <li>{{$value}}</li>
+                  @endforeach
+                </ul>
               </td>
               <td class="col-md-2">
                 {{$row->finish_with_state}}
