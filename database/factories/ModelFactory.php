299396c9928c2fa004ca510238d9c75d528b45a9
Aaron Schachter <aaronschachter@gmail.com>
2020-03-12 16:35:25 -0700

    Removes unused User factory, for now

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -3,18 +3,6 @@
 use Faker\Generator;
 use Chompy\Models\RockTheVoteReport;
 
-// @TODO: Is this used?
-$factory->define(Chompy\User::class, function (Generator $faker) {
-    static $password;
-
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
-
 $factory->define(RockTheVoteReport::class, function (Generator $faker) {
     return [
         'id' => $faker->randomNumber(2),
