280dce0adcef085df92f2106ba325736e5c9f2f5
Aaron Schachter <aaronschachter@gmail.com>
2020-03-12 11:28:45 -0700

    Adds tests for invalid apiResponse params

--- tests/Unit/Model/RockTheVoteReportTest.php
+++ tests/Unit/Model/RockTheVoteReportTest.php
@@ -2,28 +2,62 @@
 
 namespace Tests\Http;
 
-use Exception;
+use ErrorException;
 use Tests\TestCase;
 use Chompy\Models\RockTheVoteReport;
 
 class RockTheVoteReportTest extends TestCase
 {
     /**
-     * Test that expected fields are parsed from given API response.
+     * Test that expected fields are parsed from given API response object.
      *
      * @return void
      */
-    public function testCreateFromApiResponse()
+    public function testCreateFromApiResponseWithValidResponse()
     {
         $apiResponse = (object) [
             'status'=> 'queued',
             'status_url' => 'https://register.rockthevote.com/api/v4/registrant_reports/17',
         ];
+        $since = '2019-12-19 00:00:00';
+        $before = '2020-02-19 00:00:00';
 
-        $report = RockTheVoteReport::createFromApiResponse($apiResponse, null, null);
+        $report = RockTheVoteReport::createFromApiResponse($apiResponse, $since, $before);
 
         $this->assertEquals($report->id, 17);
+        $this->assertEquals($report->since, $since);
+        $this->assertEquals($report->before, $before);
         $this->assertEquals($report->status, 'queued');
         $this->assertEquals($report->user_id, null);
+    }
+
+    /**
+     * Test that an exception is thrown if a string is passed.
+     *
+     * @return void
+     */
+    public function testCreateFromApiResponseWithInvalidResponseType()
+    {
+        // Passing a string as the API response should throw an exception.
+        $this->expectException(ErrorException::class);
+
+        RockTheVoteReport::createFromApiResponse('test', null, null);
+    }
+
+    /**
+     * Test that an exception is thrown if a status_url is missing.
+     *
+     * @return void
+     */
+    public function testCreateFromApiResponseWithMissingStatusUrl()
+    {
+        // A missing status_url in the API response should throw an exception.
+        $this->expectException(ErrorException::class);
+
+        $apiResponse = (object) [
+            'status'=> 'queued',
+        ];
+
+        RockTheVoteReport::createFromApiResponse($apiResponse, null, null);
     }
 }
