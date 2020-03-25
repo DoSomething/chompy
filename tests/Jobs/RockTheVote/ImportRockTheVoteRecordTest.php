981339fa0179a81fc156a956190e5c074f241737
Aaron Schachter <aaronschachter@gmail.com>
2020-03-25 15:23:47 -0700

    Adds test for existing user

--- tests/Jobs/RockTheVote/ImportRockTheVoteRecordTest.php
+++ tests/Jobs/RockTheVote/ImportRockTheVoteRecordTest.php
@@ -26,4 +26,24 @@ class ImportRockTheVoteRecordTest extends TestCase
 
         ImportRockTheVoteRecord::dispatch($this->faker->rockTheVoteReportRow(), 42);
     }
+
+    /**
+     * Test that user is not created if user is found.
+     *
+     * @return void
+     */
+    public function testDoesNotCreateUserIfUserFound()
+    {
+        $this->mockGetNorthstarUser();
+        $this->northstarMock->shouldNotReceive('createUser');
+        $this->northstarMock->shouldNotReceive('sendPasswordReset');
+        $this->rogueMock->shouldReceive('getPost')->andReturn(null);
+        $this->rogueMock->shouldReceive('createPost')->andReturn([
+            'data' => [
+                'id' => $this->faker->randomDigitNotNull,
+            ],
+        ]);
+
+        ImportRockTheVoteRecord::dispatch($this->faker->rockTheVoteReportRow(), 42);
+    }
 }
