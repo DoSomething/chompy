dd052521bf55e6f2db33f27e5d23431bcb2ad9c9
Aaron Schachter <aaronschachter@gmail.com>
2020-04-02 05:00:33 -0700

    Removes out-of-date help text

--- resources/views/pages/users/show.blade.php
+++ resources/views/pages/users/show.blade.php
@@ -8,7 +8,6 @@
         <a href="{{config('services.rogue.url') . '/users/' . $id}}">View user in Rogue</a>
     </p>
     <h3>Rock The Vote</h3>
-    <p><strong>Note:</strong> We didn't start saving this data locally until Feb 27, 2020.</p>
     @include('pages.partials.rock-the-vote.logs', ['user_id' => $id, 'rows' => $rows])
 </div>
 
