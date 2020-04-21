4d84f96acb1a127df256906a606f8f53c2fc82b1
Aaron Schachter <aaronschachter@gmail.com>
2020-04-20 18:39:12 -0700

    Style fix

--- app/Jobs/RockTheVote/ImportRockTheVoteRecord.php
+++ app/Jobs/RockTheVote/ImportRockTheVoteRecord.php
@@ -123,7 +123,7 @@ class ImportRockTheVoteRecord implements ShouldQueue
             'post' => Arr::only($post, ['id', 'type', 'action_id', 'status', 'details']),
         ];
 
-        $userFields = ['id', 'email', 'mobile', 'voter_registration_status', 'sms_status', 'sms_subscription_topics','email_subscription_status', 'email_subscription_topics'];
+        $userFields = ['id', 'email', 'mobile', 'voter_registration_status', 'sms_status', 'sms_subscription_topics', 'email_subscription_status', 'email_subscription_topics'];
 
         foreach ($userFields as $fieldName) {
             $result['user'][$fieldName] = $user->{$fieldName};
