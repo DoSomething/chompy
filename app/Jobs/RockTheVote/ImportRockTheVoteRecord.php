b63f17d9f73a7509d4d47abd054c868b5c18177f
Aaron Schachter <aaronschachter@gmail.com>
2020-05-14 22:37:11 -0700

    Debug startedRegistration

--- app/Jobs/RockTheVote/ImportRockTheVoteRecord.php
+++ app/Jobs/RockTheVote/ImportRockTheVoteRecord.php
@@ -49,11 +49,13 @@ class ImportRockTheVoteRecord implements ShouldQueue
      */
     public function handle()
     {
-        info('Processing Rock The Vote record', ['stared_registration' => $this->record->startedRegistration]);
+        info('Processing Rock The Vote record', ['started_registration' => $this->record->startedRegistration]);
 
         $user = $this->getUser($this->userData['id'], $this->userData['email'], $this->userData['mobile']);
 
         if (! $user) {
+            info('User not found, creating user');
+
             $user = $this->createUser();
 
             $post = $this->createPost($user);
