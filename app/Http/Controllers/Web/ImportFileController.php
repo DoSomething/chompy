045e39b4c1e6a7d2d5801aed6fcb2c7020778f4f
David Furnes <david@dfurnes.com>
2020-07-13 14:35:19 -0400

    Remove unnecessary usage of 'sensitive' fields.

--- app/Http/Controllers/Web/ImportFileController.php
+++ app/Http/Controllers/Web/ImportFileController.php
@@ -44,19 +44,17 @@ class ImportFileController extends Controller
         }
 
         $data = [];
-        $userId = \Auth::user()->northstar_id;
-        $user = gateway('northstar')->asClient()->getUser($userId);
 
         if ($importType === ImportType::$rockTheVote) {
+            $userId = \Auth::user()->northstar_id;
+            $user = gateway('northstar')->asClient()->getUser($userId);
+
             $data = [
-                'addr_street1' => $user->addr_street1,
-                'addr_street2' => $user->addr_street2,
+                'display_name' => $user->display_name,
                 'addr_city' => $user->addr_city,
                 'addr_zip' => $user->addr_zip,
-                'email' => $user->email,
-                'first_name' => $user->first_name,
-                'last_name' => $user->last_name,
-                'phone' => $user->mobile,
+                'email_preview' => $user->email_preview,
+                'phone_preview' => $user->mobile_preview,
                 'tracking_source' => 'source:test,source_details:ChompyUI',
                 'started_registration' => Carbon::now()->format('Y-m-d H:i:s O'),
             ];
