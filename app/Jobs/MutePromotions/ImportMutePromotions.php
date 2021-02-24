074f5586ce15d0c6bdb06fe666a2d0fba3be415d
Aaron Schachter <aaronschachter@gmail.com>
2021-02-23 16:21:19 -0800

    Mute promotions via new API endpoint

--- app/Jobs/MutePromotions/ImportMutePromotions.php
+++ app/Jobs/MutePromotions/ImportMutePromotions.php
@@ -35,22 +35,18 @@ class ImportMutePromotions implements ShouldQueue
     }
 
     /**
-     * Execute the job to set user's promotions_muted_at field.
-     *
-     * @return array
+     * Execute the job to mute user promotions.
      */
     public function handle()
     {
-        $user = gateway('northstar')->asClient()->updateUser($this->userId, [
-            'promotions_muted_at' => Carbon::now(),
-        ]);
+        gateway('northstar')->asClient()->delete('v2/users/'. $this->userId . '/promotions');
 
         MutePromotionsLog::create([
             'import_file_id' => $this->importFile->id,
             'user_id' => $this->userId,
         ]);
 
-        info('import.mute-promotions', ['promotions_muted_at' => $user->promotions_muted_at, 'user_id' => $this->userId]);
+        info('import.mute-promotions', ['user_id' => $this->userId]);
 
         $this->importFile->incrementImportCount();
     }
